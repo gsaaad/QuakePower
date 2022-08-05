2d86c8e4774d6427a94a23b51c3fbd8dac1ccf41
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-08-05 10:32:48 -0400

    able to extract government data on largest 20 earthquakes in the world using cheerio and fetching url

--- earthquake.js
+++ earthquake.js
@@ -1,6 +1,31 @@
 // const request = require("request-promise");
 const cheerio = require("cheerio");
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
+const fs = require("fs");
 // const url =
 //   "https://earthquake.usgs.gov/earthquakes/map/?extent=-20.30342,-140.80078&extent=71.46912,-49.04297&sort=largest";
-const url = "https://en.wikipedia.org/wiki/Lists_of_earthquakes";
+// const url = "https://en.wikipedia.org/wiki/Lists_of_earthquakes";
+const url =
+  "https://www.usgs.gov/programs/earthquake-hazards/science/20-largest-earthquakes-world";
+const fetchData = async (URL) => {
+  await fetch(URL)
+    .then((response) => response.text())
+    .then((data) => {
+      // fs.writeFileSync("earthquakesGOV.text", data);
+      const $ = cheerio.load(data);
+      // console.log($("table").html());
+      const table = $("table").text();
+
+      console.log(table.split("\n"));
+      const tablearray = table.split("\n");
+      const filteredArray = [];
+      tablearray.forEach((word) => {
+        if (word.length >= 1) {
+          filteredArray.push(word);
+        }
+      });
+      console.log(filteredArray);
+      // fs.writeFileSync("tableData.txt", table);
+    });
+};
+fetchData(url);
