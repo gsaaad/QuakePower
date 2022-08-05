280e687e9760a8ab243015da4ed596a03b23e2db
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-08-04 21:34:39 -0400

    extracting earthquake table from wiki

--- earthquake.js
+++ earthquake.js
@@ -4,26 +4,3 @@ import fetch from "node-fetch";
 // const url =
 //   "https://earthquake.usgs.gov/earthquakes/map/?extent=-20.30342,-140.80078&extent=71.46912,-49.04297&sort=largest";
 const url = "https://en.wikipedia.org/wiki/Lists_of_earthquakes";
-
-// async function scrapeWebPage(url) {
-//   const response = await request.get(url);
-//   const $ = cheerio.load(response);
-//   console.log($);
-// }
-// scrapeWebPage(url);
-
-const getRawData = (URL) => {
-  return fetch(URL)
-    .then((response) => {
-      response.text();
-    })
-    .then((data) => {
-      return data;
-    });
-};
-
-const getEarthquakes = async () => {
-  const earthquakesData = await getRawData(url);
-  console.log(earthquakesData);
-};
-getEarthquakes();
