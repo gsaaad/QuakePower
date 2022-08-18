8960cf345be92452624080fc2523c6ba64ae6ad7
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-08-18 05:37:53 -0400

    modal manipulation to check user input

--- public/js/script.js
+++ public/js/script.js
@@ -21,24 +21,50 @@ const handleSubmit = (event) => {
   const earthquakeDepth = $("#depth-input").val();
   const earthquakeMagnitude = $("#magnitude-input").val();
   const earthquakeRegion = $("#region-input").val();
+
+  //   if date is empty or not string
   if (earthquakeDate === "" || typeof earthquakeDate !== "string") {
     modal.toggle();
+    console.log("Check your Date");
   }
-  if (earthquakeTime === "" || typeof earthquakeTime !== "string") {
+  //   if time is empty or not string
+  else if (earthquakeTime === "" || typeof earthquakeTime !== "string") {
     modal.toggle();
+    console.log("Check your Time");
+  } else if (earthquakeLatitude === "" || isNaN(parseInt(earthquakeLatitude))) {
+    modal.toggle();
+    console.log("Check your Latitude");
+  } else if (
+    earthquakeLongitude === "" ||
+    isNaN(parseInt(earthquakeLongitude))
+  ) {
+    modal.toggle();
+    console.log("Check your Longitude");
+  } else if (earthquakeDepth === "" || isNaN(parseInt(earthquakeDepth))) {
+    modal.toggle();
+    console.log("Check your Depth");
+  } else if (
+    earthquakeMagnitude === "" ||
+    isNaN(parseInt(earthquakeMagnitude))
+  ) {
+    modal.toggle();
+    console.log("Check your Magnitude");
+  } else if (earthquakeRegion === "" || isNaN(parseInt(earthquakeRegion))) {
+    modal.toggle();
+    console.log("Check your Region");
+  } else {
+    const earthquakeObject = {
+      earthquakeDate,
+      earthquakeTime,
+      earthquakeLatitude,
+      earthquakeLongitude,
+      earthquakeDepth,
+      earthquakeMagnitude,
+      earthquakeRegion,
+    };
+
+    console.log(earthquakeObject);
   }
-
-  const earthquakeObject = {
-    earthquakeDate,
-    earthquakeTime,
-    earthquakeLatitude,
-    earthquakeLongitude,
-    earthquakeDepth,
-    earthquakeMagnitude,
-    earthquakeRegion,
-  };
-
-  console.log(earthquakeObject);
 };
 const handleModal = (event) => {
   // close the modal
