cfc5f253a07eacfe3167d05599a93e6bd886bdb9
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-08-31 13:20:00 -0400

    completed controller for userData

--- controllers/userData-controller.js
+++ controllers/userData-controller.js
@@ -6,7 +6,7 @@ const catchError = (error) => {
 };
 
 const userDataController = {
-  // get all userData entered earthquakes
+  // !get all userData entered earthquakes
   getAllEarthquakes(req, res) {
     userData
       .find({})
@@ -14,7 +14,7 @@ const userDataController = {
       .catch(catchError(err));
   },
 
-  //   get all userData by ID
+  //   !get all userData by ID
   getEarthquakeById({ params }, res) {
     userData
       .findById({ _id: params.id })
@@ -26,6 +26,46 @@ const userDataController = {
               "No Earthquake found with this ID.. Please try a valid ID!",
           });
           return;
+        }
+        res.json(dbEarthquakeData);
+      })
+      .catch(catchError(err));
+  },
+  //   !create earthquake, userEntered
+  createEarthquake({ body }, res) {
+    userData
+      .create(body)
+      .then((dbEarthquakeData) => res.json(dbEarthquakeData))
+      .catch(catchError(err));
+  },
+  //   !updateEarthquakeByID
+  updateEarthquake({ params, body }, res) {
+    // find by id, here's the body we change, new: true, send back newer/newest version of that document
+    userData
+      .findByIdAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbEarthquakeData) => {
+        // if no earthquake data found
+        if (!dbEarthquakeData) {
+          res.status(404).json({
+            message:
+              "No Earthquake found with this ID.. Please try a valid ID!",
+          });
+        }
+        res.json(dbEarthquakeData);
+      })
+      .catch(catchError(err));
+  },
+  //   !delete earthquakeById
+  deleteEarthquake({ params }, res) {
+    userData
+      .findByIdAndDelete({ _id: params.id })
+      .then((dbEarthquakeData) => {
+        // if no earthquake data found
+        if (!dbEarthquakeData) {
+          res.status(404).json({
+            message:
+              "No Earthquake found with this ID.. Please try a valid ID!",
+          });
         }
         res.json(dbEarthquakeData);
       })
